f92b09295e71e963902bf5fd696697feaa19944b
Jan Pazdziora <jpazdziora@redhat.com>
2018-08-14 10:15:28 +0200

    Test XSLT importing.

--- test.sh
+++ test.sh
@@ -12,21 +12,26 @@ if ! [ -f tmp/x86_64/pkg2-0.0.1-1.git0f5628a6.fc28.x86_64.rpm ] || ! [ -f tmp/pk
 	rpmbuild -ba -D 'dist .fc28' -D "_srcrpmdir $(pwd)/tmp" -D "_rpmdir $(pwd)/tmp" tests/pkg2/pkg2.spec
 fi
 
-RPM2SWIDTAG_TEMPLATE_DIR=. ./rpm2swidtag.py -p tmp/x86_64/pkg1-1.2.0-1.fc28.x86_64.rpm > tmp/pkg-generated.swidtag
+# For testing, let's default the data location to the current directory
+export RPM2SWIDTAG_TEMPLATE_DIR=.
+./rpm2swidtag.py -p tmp/x86_64/pkg1-1.2.0-1.fc28.x86_64.rpm > tmp/pkg-generated.swidtag
 diff tests/pkg1/pkg1-1.2.0-1.fc28.x86_64.swidtag tmp/pkg-generated.swidtag
 
-RPM2SWIDTAG_TEMPLATE_DIR=. ./rpm2swidtag.py -p tmp/pkg1-1.2.0-1.fc28.src.rpm > tmp/pkg-generated-src.swidtag
+./rpm2swidtag.py -p tmp/pkg1-1.2.0-1.fc28.src.rpm > tmp/pkg-generated-src.swidtag
 diff tests/pkg1/pkg1-1.2.0-1.fc28.src.swidtag tmp/pkg-generated-src.swidtag
 
-RPM2SWIDTAG_TEMPLATE_DIR=. RPM2SWIDTAG_TEMPLATE=template-minimal.swidtag ./rpm2swidtag.py -p tmp/x86_64/pkg1-1.2.0-1.fc28.x86_64.rpm > tmp/pkg-from-minimal.swidtag
+RPM2SWIDTAG_TEMPLATE=template-minimal.swidtag ./rpm2swidtag.py -p tmp/x86_64/pkg1-1.2.0-1.fc28.x86_64.rpm > tmp/pkg-from-minimal.swidtag
 diff tests/pkg1/pkg1-1.2.0-1.fc28.x86_64.swidtag.minimal tmp/pkg-from-minimal.swidtag
 
-RPM2SWIDTAG_TEMPLATE_DIR=. ./rpm2swidtag.py -p tmp/x86_64/pkg2-0.0.1-1.git0f5628a6.fc28.x86_64.rpm > tmp/pkg-generated-epoch.swidtag
+RPM2SWIDTAG_XSLT=tests/xslt/swidtag.xslt ./rpm2swidtag.py -p tmp/x86_64/pkg1-1.2.0-1.fc28.x86_64.rpm > tmp/pkg-custom-tagid.swidtag
+diff tests/pkg1/pkg1-1.2.0-1.fc28.x86_64.swidtag.custom-tagid tmp/pkg-custom-tagid.swidtag
+
+./rpm2swidtag.py -p tmp/x86_64/pkg2-0.0.1-1.git0f5628a6.fc28.x86_64.rpm > tmp/pkg-generated-epoch.swidtag
 diff tests/pkg2/pkg2-0.0.1-1.git0f5628a6.fc28.x86_64.swidtag tmp/pkg-generated-epoch.swidtag
 
 rm -rf tmp/rpmdb
 rpm --dbpath $(pwd)/tmp/rpmdb --justdb --nodeps -Uvh tmp/x86_64/pkg1-1.2.0-1.fc28.x86_64.rpm
 rpm --dbpath $(pwd)/tmp/rpmdb -qa
 
-RPM2SWIDTAG_TEMPLATE_DIR=. _RPM2SWIDTAG_RPMDBPATH=$(pwd)/tmp/rpmdb ./rpm2swidtag.py pkg1 > tmp/pkg-generated.swidtag
+_RPM2SWIDTAG_RPMDBPATH=$(pwd)/tmp/rpmdb ./rpm2swidtag.py pkg1 > tmp/pkg-generated.swidtag
 diff tests/pkg1/pkg1-1.2.0-1.fc28.x86_64.swidtag tmp/pkg-generated.swidtag
---
+++ tests/xslt/swidtag.xslt
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:swid="http://standards.iso.org/iso/19770/-2/2015/schema.xsd"
+  exclude-result-prefixes="swid"
+  >
+
+<xsl:import href="../../swidtag.xslt"/>
+
+<xsl:template name="si_tagid_attr" match="swid:SoftwareIdentity/@tagId">
+  <xsl:attribute name="tagId">
+    <xsl:value-of select="$name" />
+    <xsl:text>:</xsl:text>
+    <xsl:value-of select="$epoch"/>
+    <xsl:text>:</xsl:text>
+    <xsl:value-of select="$version" />
+    <xsl:text>:</xsl:text>
+    <xsl:value-of select="$release" />
+    <xsl:text>:</xsl:text>
+    <xsl:value-of select="$arch" />
+  </xsl:attribute>
+</xsl:template>
+
+</xsl:stylesheet>
