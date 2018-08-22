60c7c5ab3e186597445da1e61677bc63715c08ec
Jan Pazdziora <jpazdziora@redhat.com>
2018-08-22 14:53:28 +0200

    Fix packaging /etc/rpm2swidtag/rpm2swidtag-tagid.xslt.

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ except ImportError:
 
 setup(
 	name = 'rpm2swidtag',
-	version = '0.2.0',
+	version = '0.2.1',
 	description = 'Exploring the rpm header information and producing SWID tag out of it',
 	author = 'Jan Pazdziora',
 	license = 'Apache License 2.0',
 	package_dir = {'': 'lib'},
 	packages = ['rpm2swidtag'],
 	scripts = ['bin/rpm2swidtag'],
-	data_files = [('/etc/rpm2swidtag', ['template.swidtag', 'swidtag.xslt', 'rpm2swidtag.xslt'])],
+	data_files = [('/etc/rpm2swidtag', ['template.swidtag', 'swidtag.xslt', 'rpm2swidtag.xslt', 'rpm2swidtag-tagid.xslt'])],
 	install_requires = ['rpm', 'lxml'],
 )
