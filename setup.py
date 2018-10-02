4f7697f24c93eb0a8a25a46f77e75aa8a8f745cd
Jan Pazdziora <jpazdziora@redhat.com>
2018-10-02 17:14:37 +0200

    Mark 0.5.0 release.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ except ImportError:
 
 setup(
 	name = 'rpm2swidtag',
-	version = '0.4.0',
-	description = 'Exploring the rpm header information and producing SWID tag out of it',
+	version = '0.5.0',
+	description = 'Tools for producing SWID tags from rpm package headers and inspecting the SWID tags',
 	author = 'Jan Pazdziora',
 	license = 'Apache License 2.0',
 	package_dir = {'': 'lib'},
