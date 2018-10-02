efbca44600f1942d565e8ad5f2b35a9f1960f2ad
Jan Pazdziora <jpazdziora@redhat.com>
2018-10-02 16:39:36 +0200

    Show suplemental tags with their primary ones.

--- lib/swidq/__init__.py
+++ lib/swidq/__init__.py
@@ -9,6 +9,7 @@ class SWIDTag:
 		self.path = file
 		self.xml = None
 		self.errors = None
+		self.supplemental_for = None
 		self.tagid = None
 		self.tagversion = None
 
@@ -22,6 +23,7 @@ class SWIDTag:
 			return
 
 		errors = []
+		supplemental_for = []
 		for si in self.xml.iter("{%s}SoftwareIdentity" % SWID_XMLNS):
 			tagid = si.get("tagId")
 			if tagid is None:
@@ -31,8 +33,23 @@ class SWIDTag:
 			self.tagversion = int(si.get("tagVersion", 0))
 			self.name = si.get("name")
 
+			for link in si.iter("{%s}Link" % SWID_XMLNS):
+				rel = link.get("rel")
+				if rel is None:
+					continue
+				href = link.get("href")
+				if href is None:
+					errors.append("file [%s] has Link with @rel='supplemental' but no @href" % file)
+					break
+				if not href.startswith("swid:"):
+					errors.append("file [%s] supplemental Link @href [%s] is not supported" % (file, href))
+					break
+				supplemental_for.append(href)
+
 		if len(errors) > 0:
 			self.errors = errors
+		if len(supplemental_for) > 0:
+			self.supplemental_for = supplemental_for
 
 	def get_path(self):
 		return self.path
@@ -52,10 +69,14 @@ class SWIDTag:
 	def get_info(self):
 		return "file [%s] tagId [%s] tagVersion [%s]" % (self.path, self.get_tagid(), self.get_tagversion())
 
+	def get_supplemental_for(self):
+		return self.supplemental_for
+
 class SWIDTagCollection:
 	def __init__(self):
 		self.by_tags = OrderedDict()
 		self.by_filenames = {}
+		self._cache_supplemental = None
 
 	def get_by_tagid(self, tagid):
 		if tagid in self.by_tags:
@@ -79,14 +100,46 @@ class SWIDTagCollection:
 		file = swidtag.get_path()
 		self.by_filenames[file] = tagid
 		self.by_tags[tagid] = swidtag
+		self._cache_supplemental = None
 		return((swidtag, False, msgs))
 
+	def compute_supplemental(self, stderr=None, prefix="", debug=False, silent=True):
+		self._cache_supplemental = {}
+		for tag in self.by_tags.values():
+			supplemental_for = tag.get_supplemental_for()
+			if not supplemental_for:
+				continue
+			for href in supplemental_for:
+				if not href.startswith("swid:"):
+					continue
+				if debug:
+					stderr.write("%smarking [%s] supplemented by [%s]\n" % (prefix, href, tag.get_tagid()))
+				if href[5:] not in self.by_tags:
+					if not silent:
+						stderr.write("%s[%s] supplements [%s] which we do not know\n" % (prefix, tag.get_tagid(), href))
+					continue
+				if href[5:] not in self._cache_supplemental:
+					self._cache_supplemental[href[5:]] = [ tag ]
+				else:
+					self._cache_supplemental[href[5:]].append(tag)
+		return self
+
+	def supplemental_for(self, tag):
+		if not self._cache_supplemental:
+			self.compute_supplemental()
+		if tag.get_tagid() in self._cache_supplemental:
+			return self._cache_supplemental[tag.get_tagid()]
+
 	def __iter__(self):
+		if not self._cache_supplemental:
+			self.compute_supplemental()
 		self._iter = iter(self.by_tags)
 		return self
 
 	def __next__(self):
 		n = next(self._iter)
+		while self.by_tags[n].get_supplemental_for():
+			n = next(self._iter)
 		if n:
 			return self.by_tags[n]
 		raise StopIteration()
--- test.sh
+++ test.sh
@@ -237,6 +237,15 @@ bin/swidq -p tests/swiddata2/pkg3.swidtag tests/swiddata1/a.test/pkg3.swidtag > 
 diff <( echo 'test.a.pkg3-1.0.0-1.x86_64 tests/swiddata2/pkg3.swidtag' ) tmp/swidq.out
 diff <( echo 'bin/swidq: skipping [tests/swiddata1/a.test/pkg3.swidtag] as existing file [tests/swiddata2/pkg3.swidtag] has already tagVersion [10]' ) tmp/swidq.err
 
+bin/swidq -p $( find tests/swiddata[12] -name '*distro*' ) tests/swiddata2/missing-tag-supplemental.swidtag > tmp/swidq.out 2> tmp/swidq.err
+diff <( echo 'test.a.Example-OS-Distro-3.x86_64 tests/swiddata1/a.test/distro.swidtag' ;
+	echo '+ test.a.Example-OS-Distro-3.14.x86_64 tests/swiddata2/distro-minor-supplemental.swidtag' ) tmp/swidq.out
+diff <( echo 'bin/swidq: [test.a.Example-OS-Distro-3.15.x86_64] supplements [swid:test.example.missing.Example-OS-Distro-3.x86_64] which we do not know' ) tmp/swidq.err
+
+bin/swidq -p tests/swiddata2/distro-minor-supplemental.swidtag > tmp/swidq.out 2> tmp/swidq.err
+diff /dev/null tmp/swidq.out
+diff <( echo 'bin/swidq: [test.a.Example-OS-Distro-3.14.x86_64] supplements [swid:test.a.Example-OS-Distro-3.x86_64] which we do not know' ) tmp/swidq.err
+
 # Testing errors
 set +e
 OUT=$( bin/swidq -p nonexistent 2>&1 )
