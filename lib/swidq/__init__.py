03d9f64f60a6e77ddfbb74026704ae316dd5a7f5
Jan Pazdziora <jpazdziora@redhat.com>
2018-10-26 12:46:27 +0200

    Add support for file: and swidpath: hrefs.

--- lib/swidq/__init__.py
+++ lib/swidq/__init__.py
@@ -1,6 +1,8 @@
 
 from lxml import etree
 from collections import OrderedDict
+from urllib.parse import urljoin
+import re
 
 SWID_XMLNS = 'http://standards.iso.org/iso/19770/-2/2015/schema.xsd'
 
@@ -18,7 +20,7 @@ class SWIDTag:
 		self.xml = None
 		self.errors = None
 		self.supplemental = False
-		self.supplemental_for = None
+		self.supplemental_for = []
 		self.tagid = None
 		self.tagversion = None
 
@@ -53,7 +55,6 @@ class SWIDTag:
 			self.supplemental = True
 
 		errors = []
-		supplemental_for = []
 
 		for link in si.iterfind("{%s}Link" % SWID_XMLNS):
 			rel = link.get("rel")
@@ -65,17 +66,19 @@ class SWIDTag:
 			href = link.get("href")
 			if href is None:
 				errors.append("file [%s] has Link with @rel='supplemental' but no @href" % file)
-				break
-			if not href.startswith("swid:"):
-				errors.append("file [%s] supplemental Link @href [%s] is not supported" % (file, href))
-				break
-			supplemental_for.append(href)
-
-		if self.supplemental:
-			if len(supplemental_for) > 0:
-				self.supplemental_for = supplemental_for
+				continue
+			m = re.search(r'^((swidpath|swid|file):|[a-z+.]+:)(.+)', href)
+			if m:
+				if m.group(2) != '':
+					self.supplemental_for.append(( m.group(2), m.group(3), href ))
+					continue
 			else:
-				errors.append("file [%s] is supplemental but does not have any supplemental Link" % file)
+				self.supplemental_for.append(( 'file', href, href ))
+				continue
+			errors.append("file [%s] supplemental Link @href [%s] is not supported" % (file, href))
+
+		if self.supplemental and len(self.supplemental_for) < 1:
+			errors.append("file [%s] is supplemental but does not have any supplemental Link" % file)
 
 		if len(errors) > 0:
 			self.errors = errors
@@ -123,6 +126,8 @@ class SWIDTagCollection:
 		return self.by_tags.get(tagid)
 
 	def load_swidtag_file(self, file):
+		if file in self.by_filenames:
+			return(( self.by_filenames[file], False, None ))
 		swidtag = SWIDTag(file)
 		if swidtag.get_errors():
 			return((None, True, swidtag.get_errors()))
@@ -136,32 +141,46 @@ class SWIDTagCollection:
 		self._cache_supplemental = None
 		return((swidtag, False, None))
 
+	def _add_to_cache_supplemental(self, supplemented, supplemental, stderr=None, debug=False, prefix=""):
+		if supplemented in self._cache_supplemental:
+			self._cache_supplemental[supplemented].append(supplemental)
+		else:
+			self._cache_supplemental[supplemented] = [ supplemental ]
+		if debug and stderr:
+			stderr.write("%smarking [%s] supplemented by [%s]\n" % (prefix, supplemented, supplemental.get_path()))
+
 	def compute_supplemental(self, stderr=None, prefix="", debug=False, silent=True):
 		self._cache_supplemental = {}
 		for tag in self.by_filenames.values():
-			supplemental_for = tag.get_supplemental_for()
-			if not supplemental_for:
-				continue
-			for href in supplemental_for:
-				if not href.startswith("swid:"):
-					continue
-				if debug:
-					stderr.write("%smarking [%s] supplemented by [%s]\n" % (prefix, href, tag.get_tagid()))
-				if href[5:] not in self.by_tags:
-					if not silent:
-						stderr.write("%s[%s] supplements [%s] which we do not know\n" % (prefix, tag.get_tagid(), href))
-					continue
-				if href[5:] not in self._cache_supplemental:
-					self._cache_supplemental[href[5:]] = [ tag ]
-				else:
-					self._cache_supplemental[href[5:]].append(tag)
+			for s in tag.get_supplemental_for():
+				if s[0] == 'swid':
+					if s[1] in self.by_tags:
+						for f in self.by_tags[s[1]]:
+							self._add_to_cache_supplemental(f, tag, stderr=stderr, prefix=prefix, debug=debug)
+						continue
+				elif s[0] == 'file':
+					t = urljoin(tag.get_path(), s[1])
+					if t in self.by_filenames:
+						self._add_to_cache_supplemental(t, tag, stderr=stderr, prefix=prefix, debug=debug)
+						continue
+				elif s[0] == 'swidpath':
+					matched = False
+					for p in self.by_filenames.values():
+						for si in p.xml.xpath(s[1], namespaces = { 'swid': SWID_XMLNS }):
+							if not si.getparent():
+								self._add_to_cache_supplemental(p.get_path(), tag, stderr=stderr, prefix=prefix, debug=debug)
+								matched = True
+					if matched:
+						continue
+				if not silent:
+					stderr.write("%s[%s] supplements [%s] which we do not know\n" % (prefix, tag.get_tagid(), s[2]))
 		return self
 
 	def supplemental_for(self, tag):
 		if not self._cache_supplemental:
 			self.compute_supplemental()
-		if tag.get_tagid() in self._cache_supplemental:
-			return self._cache_supplemental[tag.get_tagid()]
+		if tag.get_path() in self._cache_supplemental:
+			return self._cache_supplemental[tag.get_path()]
 		return []
 
 	def __iter__(self):
