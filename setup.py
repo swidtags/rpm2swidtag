4d541b7e9faafb75c037c85b9a49022f5bfcc39b
Jan Pazdziora <jpazdziora@redhat.com>
2019-09-19 13:51:16 +0200

    Remove the GPG key for Fedora 28 which is EOL.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
 	scripts = ['bin/rpm2swidtag', 'bin/dnf-plugin-swidtags-update-from-0.7', 'bin/swidq'],
 	data_files = [
 		('/etc/rpm2swidtag', ['rpm2swidtag.conf', 'swidtag-template.xml', 'swidtag.xslt', 'rpm2swidtag.xslt', 'rpm2swidtag-tagid.xslt']),
-		('/etc/rpm2swidtag/rpm2swidtag.conf.d', ['rpm2swidtag.conf.d/fedora-rawhide.conf', 'rpm2swidtag.conf.d/fedora-30.conf', 'rpm2swidtag.conf.d/fedora-29.conf', 'rpm2swidtag.conf.d/fedora-28.conf']),
+		('/etc/rpm2swidtag/rpm2swidtag.conf.d', ['rpm2swidtag.conf.d/fedora-rawhide.conf', 'rpm2swidtag.conf.d/fedora-30.conf', 'rpm2swidtag.conf.d/fedora-29.conf']),
 		('/etc/swid', ['swidq.conf']),
 		('/etc/swid/swidtags.d', []),
 		('/usr/share/swidq/stylesheets', ['swidq-info.xslt', 'swidq-dump.xslt', 'swidq-files.xslt', 'swidq-xml.xslt']),
