9c1555d2ac566a2f2a2dfeaa3c2fb6fa7d5d63db
Jan Pazdziora <jpazdziora@redhat.com>
2020-01-06 12:37:56 +0100

    Workaround 1788084.

--- tests/build.sh
+++ tests/build.sh
@@ -5,6 +5,8 @@ set -x
 
 DNF=dnf
 $DNF install -y rpm-build make "$DNF-command(builddep)"
+# Workaround 1788084
+$DNF install -y 'python3-astroid <> 2.3.3-2.gitace7b29.fc31'
 make spec
 $DNF builddep -y dist/rpm2swidtag.spec
 make rpm
