b46b028eb352427c445043145c74a91d3e240593
Jan Pazdziora <jpazdziora@redhat.com>
2020-01-10 14:11:29 +0100

    Revert "Workaround 1788084."
    This reverts commit 9c1555d2ac566a2f2a2dfeaa3c2fb6fa7d5d63db.
    
    The bug was resolved with new builds.

--- tests/build.sh
+++ tests/build.sh
@@ -5,8 +5,6 @@ set -x
 
 DNF=dnf
 $DNF install -y rpm-build make "$DNF-command(builddep)"
-# Workaround 1788084
-grep 'Fedora release 31' /etc/fedora-release && $DNF install -y 'python3-astroid <> 2.3.3-2.gitace7b29.fc31'
 make spec
 $DNF builddep -y dist/rpm2swidtag.spec
 make rpm
