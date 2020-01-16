286342cbdbc5a2ca62e94ebceb640df719f74d0d
Jan Pazdziora <jpazdziora@redhat.com>
2020-01-16 10:40:07 +0100

    Add Travis CI testing on CentOS.

--- tests/build.sh
+++ tests/build.sh
@@ -5,6 +5,8 @@ set -x
 
 DNF=dnf
 $DNF install -y rpm-build make "$DNF-command(builddep)"
+test -f /etc/centos-release && $DNF install -y python3 epel-release
+
 make spec
 $DNF builddep -y dist/rpm2swidtag.spec
 make rpm
