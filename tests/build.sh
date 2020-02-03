891713f36c45a89a7a9ea379abe1aaf9808c11c8
Jan Pazdziora <jpazdziora@redhat.com>
2020-02-03 10:20:22 +0100

    Revert "Workaround 1788115."
    This reverts commit 3f91c0d891223f9f30af5964c5c0030a03c69641.
    
    The python3-pylint dependencies have been fixed in EPEL 8.

--- tests/build.sh
+++ tests/build.sh
@@ -6,7 +6,6 @@ set -x
 DNF=dnf
 $DNF install -y rpm-build make "$DNF-command(builddep)"
 test -f /etc/centos-release && $DNF install -y python3 epel-release https://kojipkgs.fedoraproject.org//packages/fakechroot/2.20.1/2.el8/x86_64/fakechroot-{,libs-}2.20.1-2.el8.x86_64.rpm
-test -f /etc/centos-release && $DNF install -y --enablerepo=epel-testing python3-astroid
 
 make spec
 $DNF builddep -y dist/rpm2swidtag.spec
