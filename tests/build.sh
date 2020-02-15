e00062f9f74506a652340f449932513ef2e13916
Jan Pazdziora <jpazdziora@redhat.com>
2020-02-15 22:49:50 +0100

    Revert workaround 1791626 - fakechroot is now in EPEL 8.

--- tests/build.sh
+++ tests/build.sh
@@ -6,7 +6,6 @@ set -x
 DNF=dnf
 $DNF install -y rpm-build make "$DNF-command(builddep)"
 test -f /etc/centos-release && $DNF install -y python3 epel-release
-test -f /etc/centos-release && $DNF install -y --enablerepo=epel-testing fakechroot
 
 make spec
 $DNF builddep -y dist/rpm2swidtag.spec
