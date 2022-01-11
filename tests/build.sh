9941e106c1bdc5aa4cc89fa2379824769de6fd81
Jan Pazdziora <jpazdziora@redhat.com>
2022-01-11 09:53:10 +0100

    Force the use of setuptools as distutils is now deprecated.
    Addressing
    /src/setup.py:6: DeprecationWarning: The distutils package is deprecated and slated for removal in Python 3.12. Use setuptools or check PEP 632 for potential alternatives
      from distutils.core import setup

--- tests/build.sh
+++ tests/build.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 DNF=dnf
-$DNF install -y rpm-build make "$DNF-command(builddep)"
 test -f /etc/centos-release && $DNF install -y python3 epel-release
+$DNF install -y rpm-build make "$DNF-command(builddep)" python3-setuptools
 
 make spec
 $DNF builddep -y dist/rpm2swidtag.spec
