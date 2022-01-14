be0b6b7bee87a1225d3d1957796a53bd41edbed4
Jan Pazdziora <jpazdziora@redhat.com>
2022-01-14 16:41:32 +0100

    Mark 0.8.13 release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 	name = 'rpm2swidtag',
-	version = '0.8.12',
+	version = '0.8.13',
 	description = 'Tools for producing SWID tags from rpm package headers and inspecting the SWID tags',
 	author = 'Jan Pazdziora',
 	author_email = 'jpazdziora@redhat.com',
